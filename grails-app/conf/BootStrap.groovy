1ac453c4b0506d8e58fcaecce8983516ae74a650
Andrew Mccoy <amccoy2@gmail.com>
2012-12-04 22:34:24 -0500

    Committing Security Config Changes

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,47 +1,41 @@
-import java.text.ParsePosition
-import java.text.SimpleDateFormat;
-
 import grails.util.Environment
 
-import org.grails.plugins.springsecurity.service.AuthenticateService
-import org.springframework.context.annotation.Profile
+import java.text.SimpleDateFormat
 
 import com.grailsinaction.*
 import com.rams.*
 
 class BootStrap {
-	def authenticateService
-	def init = { servletContext ->
-		switch (Environment.current) {
-			case Environment.DEVELOPMENT:
-				createAdminUserIfRequired()
-				break;
-			case Environment.PRODUCTION:
-				println "No special configuration required"
-				break;
-		}
+    def authenticateService
+    def init = { servletContext ->
+	switch (Environment.current) {
+	    case Environment.DEVELOPMENT:
+		createAdminUserIfRequired()
+		break;
+	    case Environment.PRODUCTION:
+		println "No special configuration required"
+		break;
 	}
-	def destroy = {
-	}
-	void createAdminUserIfRequired() {
-		SimpleDateFormat dateFormatter = new SimpleDateFormat()
+    }
+    def destroy = {
+    }
+    void createAdminUserIfRequired() {
+	SimpleDateFormat dateFormatter = new SimpleDateFormat()
 
-		if (!User.findByEid("000000")) {
-			println "Fresh Database. Creating ADMIN user."
-			Role role = new Role(authority: "ROLE_ADMIN", description: "A super user.")
-			User user = new User(eid: "000000",
-					password: authenticateService.encodePassword("password"), lname: "admin", fname: "an",
-					address: "a house", city: "New Orleans", zipcode: "06712",
-					state: "LA", phoneNumber: "1234567890")
-			user.save()
-			println (role.validate())
-			role.addToPeople(user)
-			role.save()
-			println (user.eid + user.password)
-			
-		
-		} else {
-			println "Existing admin user, skipping creation"
-		}
+	if (!User.findByEid("000000")) {
+	    println "Fresh Database. Creating ADMIN user."
+	    Role role = new Role(authority: "ROLE_ADMIN", description: "A super user.")
+	    User user = new User(eid: "000000",
+		    password: authenticateService.encodePassword("password"), lname: "admin", fname: "an",
+		    address: "a house", city: "New Orleans", zipcode: "06712",
+		    state: "LA", phoneNumber: "1234567890")
+	    user.save()
+	    println (role.validate())
+	    role.addToPeople(user)
+	    role.save()
+	    println (user.eid +"\t"+  user.password)
+	} else {
+	    println "Existing admin user, skipping creation"
 	}
+    }
 }
--- grails-app/conf/SecurityConfig.groovy
+++ grails-app/conf/SecurityConfig.groovy
@@ -1,18 +1,19 @@
 security {
 
-	// see DefaultSecurityConfig.groovy for all settable/overridable properties
+    // see DefaultSecurityConfig.groovy for all settable/overridable properties
 
-	active = true
+    active = true
 
-	loginUserDomainClass = "com.rams.User"
-	authorityDomainClass = "com.rams.Role"
-	requestMapClass = "Requestmap"
-	userName= "eid"
-	password = "password"
+    loginUserDomainClass = "com.rams.User"
+    authorityDomainClass = "com.rams.Role"
+    requestMapClass = "Requestmap"
+    userName= "eid"
+    password = "password"
 
-	useRequestMapDomainClass = false
+    useRequestMapDomainClass = false
 
-	requestMapString = """\
+    requestMapString = """\
+        /**=IS_AUTHENTICATED_ANONYMOUSLY
 	CONVERT_URL_TO_LOWERCASE_BEFORE_COMPARISON
 	PATTERN_TYPE_APACHE_ANT
 	/=IS_AUTHENTICATED_ANONYMOUSLY
@@ -21,12 +22,12 @@ security {
 	/css/**=IS_AUTHENTICATED_ANONYMOUSLY
 	/images/**=IS_AUTHENTICATED_ANONYMOUSLY
 	/plugins/**=IS_AUTHENTICATED_ANONYMOUSLY
-	/**=IS_AUTHENTICATED_FULLY
 	/user/**=ROLE_ADMIN
 	/role/**=ROLE_ADMIN
+    	/rams/**=ROLE_ADMIN
 	/requestmap/**=ROLE_ADMIN
 	"""
-	
-	loginFormUrl = "/login/auth"
-	defaultTargetUrl = "/"
+
+    loginFormUrl = "/login/auth"
+    defaultTargetUrl = "/rams"
 }
